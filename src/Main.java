40e190ec21a94ae08ff47d778e7cee8660d60621
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-21 21:58:23 +0700

    Add while loop to get even number sum

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main (String [] args){
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456789));
+        System.out.println(EvenDigitSum.getEvenDigitSum(252));
+        System.out.println(EvenDigitSum.getEvenDigitSum(-22));
+    }
 }
+
+//* getEvenDigitSum(123456789); → should return 20 since 2 + 4 + 6 + 8 = 20
+//
+//* getEvenDigitSum(252); → should return 4 since 2 + 2 = 4
+//
+//* getEvenDigitSum(-22); → should return -1 since the number is negative
